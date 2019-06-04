d71d9b3f6a736f197ca9486cdd8c56f371f0ba8b
Miguel Pinzón <36303361+cmpinzonh@users.noreply.github.com>
2019-06-04 09:09:11 -0500

    Added a test uploading a file (#173)
    * Added a test uploading a file
    
    * Changed variable names
    
    * Changed a selector
    
    * Added descriptive names for variables

--- src/page/personal-information.page.ts
+++ src/page/personal-information.page.ts
@@ -1,4 +1,7 @@
-import { element, by, ElementFinder } from 'protractor';
+import { browser, element, by, ElementFinder } from 'protractor';
+import { resolve } from 'path';
+import { existsSync } from 'fs';
+import * as remote from 'selenium-webdriver/remote';
 
 interface PersonalInformation {
   firstName: string;
@@ -9,6 +12,7 @@ interface PersonalInformation {
   tools: string[];
   continent: string;
   commands: string[];
+  file?: string;
 }
 
 export class PersonalInformationPage {
@@ -16,12 +20,14 @@ export class PersonalInformationPage {
   private lastNameField: ElementFinder;
   private sendButton: ElementFinder;
   private pageTitleLabel: ElementFinder;
+  private uploadFileInput: ElementFinder;
 
   constructor() {
     this.firstNameField = element(by.name('firstname'));
     this.lastNameField = element(by.name('lastname'));
     this.sendButton = element(by.id('submit'));
     this.pageTitleLabel = element(by.css('#content h1'));
+    this.uploadFileInput = element(by.id('photo'));
   }
 
   private getSexOption(sex: string): ElementFinder {
@@ -36,7 +42,7 @@ export class PersonalInformationPage {
     return element(by.css(`[name="profession"][value="${profession}"]`));
   }
 
-  private getToolsRadioButton(tool: string): ElementFinder {
+  private getToolRadioButton(tool: string): ElementFinder {
     return element(by.css(`[name="tool"][value="${tool}"]`));
   }
 
@@ -52,6 +58,21 @@ export class PersonalInformationPage {
     return await this.pageTitleLabel.getText();
   }
 
+  public async getFilename(): Promise<string> {
+    const fullPath: string = await this.uploadFileInput.getAttribute('value');
+    return fullPath.split(/(\\|\/)/g).pop();
+  }
+
+  private async uploadFile(relativePath: string): Promise<void> {
+    const fullPath = resolve(process.cwd(), relativePath);
+
+    if (existsSync(fullPath)) {
+      await browser.setFileDetector(new remote.FileDetector());
+      await this.uploadFileInput.sendKeys(fullPath);
+      await browser.setFileDetector(undefined);
+    }
+  }
+
   public async fillForm(form: PersonalInformation): Promise<void> {
     await this.firstNameField.sendKeys(form.firstName);
     await this.lastNameField.sendKeys(form.lastName);
@@ -62,9 +83,12 @@ export class PersonalInformationPage {
       await this.getProfessionRadioButton(profession).click();
     }
 
-    for (const tool of form.tools) {
-      await this.getToolsRadioButton(tool).click();
+    if (form.file) {
+      await this.uploadFile(form.file);
+    }
 
+    for (const tool of form.tools) {
+      await this.getToolRadioButton(tool).click();
     }
 
     await this.getContinentChoice(form.continent).click();
